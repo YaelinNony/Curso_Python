f44a044cb521a16ed8e157293160b7ecaf31cc9d
Yaelin Sánchez <168676463+YaelinNony@users.noreply.github.com>
2025-11-13 17:42:47 -0600

    Add files via upload

---
+++ Modulo 3/Clase 1/Prueba.py
@@ -0,0 +1,15 @@
+# Importar todas las librerías que instalamos
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import jupyter
+
+# Imprimir un mensaje de éxito y las versiones
+print("¡Todo instalado correctamente!")
+print(f"NumPy versión: {np.__version__}")
+print(f"Pandas versión: {pd.__version__}")
+print(f"Matplotlib versión: {plt.matplotlib.__version__}")
+print(f"Seaborn versión: {sns.__version__}")
+
+#---- fin del código ----
